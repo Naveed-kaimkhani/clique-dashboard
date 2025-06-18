a71a3e3ead1985c2e6a289cb0922034bb3376883
naveed_kk <naveedkaimkhami@gmail.com>
2025-06-18 06:53:25 +0500

    .

--- lib/view/navigation_bar.dart
+++ lib/view/navigation_bar.dart
@@ -8,8 +8,6 @@ import 'package:post_krakren_dashboard/view/chat_screens/chat_list.dart';
 import 'package:post_krakren_dashboard/view/dashboard_home/dashboard_home.dart';
 import 'package:post_krakren_dashboard/view/users_influencers/user_influencers_screens.dart';
 import 'package:post_krakren_dashboard/view/products_screen/product_page.dart';
-import 'package:post_krakren_dashboard/view/videos_approval/delete_popstream.dart';
-import 'package:post_krakren_dashboard/view/videos_approval/videos_approval.dart';
 import '../components/index.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -27,8 +25,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
     DashboardHome(),
     UsersInfluencersScreen(),
     ProductPage(),
-    VideosApprovalScreen(),
-    DeletePopstream(),
+    // VideosApprovalScreen(),
+    // DeletePopstream(),
     ChatList(),
   ];
 
@@ -68,9 +66,9 @@ class _DashboardScreenState extends State<DashboardScreen> {
         _drawerItem(Icons.dashboard, "Dashboard", 0),
         _drawerItem(Icons.group, "Users / Influencers", 1),
         _drawerItem(Icons.calendar_today, "Product Details", 2),
-        _drawerItem(Icons.chat, "Videos Decision", 3),
-        _drawerItem(Icons.chat, "Approved Popstreams", 4),
-        _drawerItem(Icons.settings, "Cliques", 5),
+        // _drawerItem(Icons.chat, "Videos Decision", 3),
+        // _drawerItem(Icons.chat, "Approved Popstreams", 3),
+        _drawerItem(Icons.settings, "Cliques", 4),
       ],
     );
   }
